34124e87dae7ce866d82a070bc9de7eae656d6bc
seanlanders92 <seanlanders92@gmail.com>
2020-01-13 21:04:30 -0800

    fixed total output value

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ var seattleStore = {
     max: 65,
     avgCookies: 6.3,
     cookiesales:[],
+
     cookieSales: function cookieSales(){
-    var hours = ['6am:','7am:','8am:','9am:','10am','11am:','12pm:','1pm','2pm:','3pm:','4pm:','5pm:','6pm:','7pm:'];
+    var hours = ['6am: ','7am: ','8am: ','9am: ','10am ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ','6pm: ','7pm: '];
        for(var i=0; i < hours.length; i++){
         var avg = Math.floor(getRandomInt(this.min,this.max) * this.avgCookies);
         this.cookiesales.push(avg);
@@ -24,7 +25,14 @@ var seattleStore = {
         seattleStoreUL.append(newTag);
             }
     return `${hours}, ${this.cookiesales} cookies`;
+        },
+    calculateTotal: function sumTotal(){
+        var sum = 0;
+        for(var i=0; i< this.cookiesales.length; i++){
+            sum = sum + this.cookiesales[i];
         }
+        return sum;
+    }
 }
 var tokyoStore = {
     min: 3,
@@ -88,9 +96,10 @@ var seattleStoreArticle = document.getElementById('seattleStore');
 
 var seattleStoreUL = document.createElement('ul');
 seattleStoreUL.textContent = seattleStore.name;
-// var avgLI = document.createElement('li');
-// avgLI.textContent = seattleStore.cookieSales()+ '.';
-// seattleStoreUL.appendChild(avgLI);
 seattleStoreArticle.append(seattleStoreUL);
 
 seattleStore.cookieSales();
+
+var avgLI = document.createElement('li');
+avgLI.textContent = `${seattleStore.calculateTotal()}` + '.';
+seattleStoreUL.appendChild(avgLI);
